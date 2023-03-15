458de927772762b8596fbbfcb2b18bbca05536b0
Flukus99 <elementary@outlook.es>
2023-03-15 10:11:28 -0500

    añadiendo contraseña

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const soloL =/^[a-z]+\D$/gi
-const formato=/\d{2}[/]\d{2}[/]\d{4}/gi
+const formato=/[0-3]{1}[0-9]{1}[/][0]{1}[0-9]{1}|[1]{1}[0-2]{1}[/]\d{4}/gi
 const telefon=/[3]\d{9}$/
 const correo=/^[a-z]+[0-9]+@[a-z]+\.[a-z]/gi
 let apellidos=''
@@ -7,6 +7,7 @@ let nombres=''
 let formatos=''
 let telefonos=''
 let correoss=''
+let contraseñas=''
 registrar.addEventListener('click',()=>{
     
     nombres = soloL.test(nombre.value)
@@ -14,6 +15,7 @@ registrar.addEventListener('click',()=>{
     formatos=formato.test(fecha.value)
     telefonos=telefon.test(telefono.value)
     correoss=correo.test(email.value)
+    
     // if (nombres == true){
     //  } else{
     //     alert('nombre incorrecto')
@@ -35,9 +37,15 @@ registrar.addEventListener('click',()=>{
     // }else{
     //     console.log('inavlido telefono')
     // }
-    if(correoss == true){
-        console.log('listo el telefono')
+    // if(correoss == true){
+    //     console.log('listo el telefono')
+    // }else{
+    //     console.log('inavlido telefono')
+    // }
+    if(contraseña.value === contraseña2.value){
+        console.log('la contraseña es igual')
+        contraseñas=true
     }else{
-        console.log('inavlido telefono')
+        console.log('invalido contraseña')
     }
 })
